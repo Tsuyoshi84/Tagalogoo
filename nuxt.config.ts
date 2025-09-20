9b562662bdb4c8b83640cdaa7e48d17418ec84e4
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2025-09-20 19:15:31 +0900

    feat: Install and configure Tailwind CSS 4, Biome, and Lefthook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
 
 	modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/test-utils', '@nuxt/image', '@vueuse/nuxt'],
+	css: ['~/assets/css/main.css'],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	eslint: {
 		config: {
 			nuxt: {
