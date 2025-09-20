64175d55129208a55a3c19f1712564efb5652b1c
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2025-09-20 23:31:37 +0900

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,29 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
-
 	modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/test-utils', '@nuxt/image', '@vueuse/nuxt'],
 	css: ['~/assets/css/main.css'],
 	vite: {
 		plugins: [tailwindcss()],
+	},
+	typescript: {
+		tsConfig: {
+			compilerOptions: {
+				allowImportingTsExtensions: true,
+				allowUnreachableCode: false,
+				erasableSyntaxOnly: true,
+				forceConsistentCasingInFileNames: true,
+				noErrorTruncation: true,
+				noFallthroughCasesInSwitch: true,
+				noImplicitOverride: true,
+				noImplicitReturns: true,
+				noUncheckedIndexedAccess: true,
+				noUnusedLocals: false,
+				noUnusedParameters: false,
+				useUnknownInCatchVariables: true,
+				strict: true,
+			},
+		},
 	},
 	eslint: {
 		config: {
