391b9301b7839ce5b92df0d3ee8c7052cafc071e
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2025-09-20 23:40:57 +0900

    Fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,10 @@
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
-	compatibilityDate: '2025-07-15',
-	devtools: { enabled: true },
 	modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/test-utils', '@nuxt/image', '@vueuse/nuxt'],
+	devtools: { enabled: true },
 	css: ['~/assets/css/main.css'],
+	compatibilityDate: '2025-07-15',
 	vite: {
 		plugins: [tailwindcss()],
 	},
