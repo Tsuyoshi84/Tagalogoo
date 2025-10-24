0cf1d85231aceddd5da3d9de6f1c4bc6c07e775b
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2025-10-24 14:10:02 +0900

    refactor: remove consolidated conjugate tests in favor of individual files

--- shared/utils/word/conjugate.spec.ts
+++
@@ -1,9 +0,0 @@
-// This file imports and re-exports all focus-specific tests for the conjugate function
-// Individual tests are now organized in separate files:
-// - conjugateMag.spec.ts - Tests for MAG focus
-// - conjugateUm.spec.ts - Tests for UM focus
-// - conjugateIn.spec.ts - Tests for IN focus
-
-import './conjugateMag.spec.ts'
-import './conjugateUm.spec.ts'
-import './conjugateIn.spec.ts'
